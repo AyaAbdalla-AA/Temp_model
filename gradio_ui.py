a582b6e163c2a70c4b0aab9d4b06413157750ab2
AyaAbdalla-AA <qsc-digi-amaaa@queensu.ca>
2026-05-07 18:42:56 +0300

    Create gradio_ui.py

---
+++ gradio_ui.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import requests
+
+API_URL = "http://127.0.0.1:8000/predict"
+
+
+def predict(temp):
+    response = requests.post(
+        API_URL,
+        json={"X_fahrenheit": temp}
+    )
+
+    result = response.json()
+    return result["prediction"]
+
+
+# Gradio interface
+iface = gr.Interface(
+    fn=predict,
+    inputs=gr.Number(label="Fahrenheit Temperature"),
+    outputs=gr.Textbox(label="Prediction"),
+    title="🌡️ MLflow Temperature Predictor",
+    description="Enter a temperature in Fahrenheit and get ML model prediction."
+)
+
+iface.launch()
