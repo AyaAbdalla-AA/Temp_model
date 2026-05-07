248edb0d15f566e0d3c95e6484b2c282c3236f9a
AyaAbdalla-AA <qsc-digi-amaaa@queensu.ca>
2026-05-07 18:08:01 +0300

    Add health check test and fix assertion

--- test_main.py
+++ test_main.py
@@ -3,6 +3,12 @@ from main import app
 
 client = TestClient(app)
 
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
 def test_predict():
     response = client.post(
         "/predict",
