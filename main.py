bb33d24909adeab98b3ee7b3afc7dd7e8fcd8836
AyaAbdalla-AA <qsc-digi-amaaa@queensu.ca>
2026-05-07 17:06:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mlflow.pyfunc
+import pandas as pd
+
+app = FastAPI()
+
+logged_model = 'runs:/59a3729a0eed411a95fa84a9b3150361/model'
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+
+class PredictionInput(BaseModel):
+    X_fahrenheit: float
+    # feature2: float
+    # feature3: float
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.post("/predict")
+async def predict(data: PredictionInput):
+    # Convert input to DataFrame
+    features = pd.DataFrame([{
+        "X_fahrenheit": data.X_fahrenheit }])
+    prediction = loaded_model.predict(pd.DataFrame(features))
+
+
+    return {
+        "prediction": prediction.tolist()
+    }
