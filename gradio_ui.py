ade5a743497bd6482a531f72af38666587f69432
AyaAbdalla-AA <qsc-digi-amaaa@queensu.ca>
2026-05-08 00:06:47 +0300

    Fix prediction return value and update slider step

--- gradio_ui.py
+++ gradio_ui.py
@@ -11,16 +11,16 @@ def predict(temp):
     )
 
     result = response.json()
-    return result["prediction"][0][0]
+    return result["prediction"][0]
 
 
 # Gradio interface
 iface = gr.Interface(
     fn=predict,
-    inputs=gr.Slider(label="Fahrenheit Temperature", minimum=-10, maximum=60 , step=0.),
+    inputs=gr.Slider(label="Fahrenheit Temperature", minimum=-10, maximum=60 , step=1),
     outputs=gr.Textbox(label="Prediction"),
     title="🌡️ MLflow Temperature Predictor",
     description="Enter a temperature in Fahrenheit and get ML model prediction."
 )
 
-iface.launch( share=True)
+iface.launch(server_name="0.0.0.0", server_port=7860, share=False)
