668121d75d12684491e07b6f29a27b984b62fcc3
AyaAbdalla-AA <qsc-digi-amaaa@queensu.ca>
2026-05-07 17:10:51 +0300

    Add test for predict endpoint in test_main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_predict():
+    response = client.post(
+        "/predict",
+        json={"X_fahrenheit": 100}
+    )
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
